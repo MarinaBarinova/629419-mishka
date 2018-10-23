8c2a512c0840e2d162d266ff195861550de58b14
Марина <marina7solnce@gmail.com>
2018-10-24 02:52:01 +0300

    исправлено скрытие меню в мобильной версии

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,9 +1,14 @@
 let navMain = document.querySelector(".main-nav");
 let navToggle = document.querySelector(".main-nav__toggle");
-navMain.classList.remove("main-nav--nojs");
+if(navMain.classList.contains("main-nav--nojs")) {
+  navMain.classList.remove("main-nav--nojs");
+  hideMenu();
+}
 //let contactsMap = document.querySelector(".contacts__map--no-js");
 //contactsMap.classList.remove("contacts__map--no-js");
-navToggle.addEventListener("click", function () {
+navToggle.addEventListener("click", hideMenu);
+
+function hideMenu() {
   if (navMain.classList.contains("main-nav--closed")) {
     navMain.classList.remove("main-nav--closed");
     navMain.classList.add("main-nav--opened");
@@ -11,4 +16,4 @@ navToggle.addEventListener("click", function () {
     navMain.classList.add("main-nav--closed");
     navMain.classList.remove("main-nav--opened");
   }
-})
+}
