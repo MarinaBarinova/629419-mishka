13f599f966783553f6065946dc1eb1169f3f734c
Марина <marina7solnce@gmail.com>
2018-10-24 13:36:44 +0300

    добавлены ссылки в каталоге и на кнопку заказа, стиль disabled для радиокнопок и чекбоксов

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,13 +1,10 @@
 let navMain = document.querySelector(".main-nav");
 let navToggle = document.querySelector(".main-nav__toggle");
-navMain.classList.remove("main-nav--nojs");
-/*if(navMain.classList.contains("main-nav--nojs")) {
+//navMain.classList.remove("main-nav--nojs");
+if(navMain.classList.contains("main-nav--nojs")) {
   navMain.classList.remove("main-nav--nojs");
   hideMenu();
 }
-//let contactsMap = document.querySelector(".contacts__map--no-js");
-//contactsMap.classList.remove("contacts__map--no-js");
-*/
 navToggle.addEventListener("click", hideMenu);
 
 function hideMenu() {
